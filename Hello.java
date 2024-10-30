e750df568436823d689ed3603f0677bf4a07f6ce
chi-stack <chrisgitonga49@gmail.com>
2024-10-30 14:38:36 +0300

    second commit

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello {
+
+    //starting point of a java application
+    public static void main(String []arguments) {
+
+        //standard output stream
+        System.out.println("My name is Chris");
+
+    }
+    
+}
